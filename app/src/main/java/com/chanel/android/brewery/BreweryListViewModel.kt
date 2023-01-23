345471a86e35978d8768a4fa5faa2fdff94c2847
Chanel Mendoza <chanelmdza@gmail.com>
2023-01-23 14:49:36 -0800

    Set up view model

---
+++ app/src/main/java/com/chanel/android/brewery/BreweryListViewModel.kt
@@ -0,0 +1,45 @@
+package com.chanel.android.brewery
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.chanel.android.brewery.model.Brewery
+import com.chanel.android.brewery.network.RetrofitInstance
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+
+private const val TAG = "BreweryListViewModel"
+
+class BreweryListViewModel: ViewModel() {
+
+    private var _breweries = MutableLiveData<List<Brewery>>()
+    val breweries: LiveData<List<Brewery>>
+        get() = _breweries
+
+    private val disposables = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
+
+    fun getBreweries() {
+        RetrofitInstance.breweryAPI.getBreweries()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { response ->
+                    _breweries.postValue(response)
+                },
+                onError = { error ->
+                    Log.d(TAG, "Error getting breweries: ${error.message}")
+                }
+            )
+            .addTo(disposables)
+    }
+    
+}
--- app/src/main/java/com/chanel/android/brewery/network/RetrofitInstance.kt
+++ app/src/main/java/com/chanel/android/brewery/network/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 private const val BASE_URL = "https://api.openbrewerydb.org/breweries/"
-class RetrofitInstance {
+object RetrofitInstance {
 
     val breweryAPI: BreweryAPI by lazy {
         Retrofit.Builder()
